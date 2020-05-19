f8dcba4b6976ed780e92d5d547ad94a881128b6e
github username <github email address>
2020-05-19 16:24:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(sourceArray, fn) {
+  const result = [];
+  for (const element of sourceArray) {
+    result.push(fn(element));
+  }
+  return result;
+}
+
+function reduce(sourceArray, fn, startingPoint) {
+  let total = !!startingPoint ? startingPoint : sourceArray[0];
+  const startIndex = !!startingPoint ? 0 : 1;
+
+  for (let i = startIndex; i < sourceArray.length; i++) {
+    total = fn(sourceArray[i], total);
+  }
+  return total
+}
